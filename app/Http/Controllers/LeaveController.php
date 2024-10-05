e97b3dbfa63272000c0859d6f805c7ce5a3da4b4
Osama13088 <firebase946@gmail.com>
2024-10-05 06:11:16 +0500

    done

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -8,7 +8,21 @@ class LeaveController extends Controller
 {
     public function allLeaves()
     {
-        $leaves = Leaves::where('final_status','pending')->latest()->get();
+        $leaves = Leaves::all();
         return view('leaves.companyleaves',compact('leaves'));
+    }
+
+    public function approve(Request $request)
+    {
+        $leave = Leaves::find($request->id);
+        $leave->update(['final_status' => 'approved', 'hr_status' => 'approved']);
+        return 'success';
+    }
+
+    public function reject(Request $request)
+    {
+        $leave = Leaves::find($request->id);
+        $leave->update(['final_status' => 'rejected', 'hr_status' => 'rejected']);
+        return 'success';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,8 @@ Route::middleware('auth')->group(function () {
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'leaves','as'=>'leaves.'], function (){
     Route::get('all-leaves',[LeaveController::class,'allLeaves'])->name('allLeaves');
+    Route::post('approveLeave', [LeaveController::class, 'approve'])->name('approveLeave');
+    Route::post('rejectLeave', [LeaveController::class, 'reject'])->name('rejectLeave');
 });
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'attendance','as'=>'attendance.'], function (){
